e56c481c7aadae499f6e2a3d2a1e551d735752a4
AkuFranssila <akufra@gmail.com>
2019-10-17 21:13:15 +0300

    feature-designed_player_parsing
    Added files ready for player parsing

---
+++ helpers_data_management.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+import json
+import logging
+
+
+#Class
+#Contains function to open files
+    #if **kwargs are list then loop through all lists
+
+
+#Open & loop file(s)
+#Send data to mongo
+#Save file
+#Find file
---
+++ parse_player.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+import json
+import logging
+from mongoengine import connect
+from schemas import Player
