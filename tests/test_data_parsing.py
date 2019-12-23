b95b147f41240a4c4c177b3343c67447049bb74a
AkuFranssila <akufra@gmail.com>
2019-12-23 09:45:35 +0200

    feature-add_first_test
    Do some config changes to circelci and and first test for it

---
+++ tests/test_data_parsing.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import logging
+import unittest
+import sys
+sys.path.append('/pdga_ratings/pdga_data')
+from helpers_data_parsing import *
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+class TestDataParsers(unittest.TestCase):
+
+    def test_fullname_parser(self):
+        self.assertEqual(ParseFullName("Clark Kent"), ('Clark', 'Kent'))
+
+    # def test_upper(self):
+    #     self.assertEqual('foo'.upper(), 'FOO')
+    #
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+    #
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
