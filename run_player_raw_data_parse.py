f0448fd59ae55a670115000d052008336efad0a8
AkuFranssila <akufra@gmail.com>
2020-01-21 21:03:43 +0200

    fix-styling_for_player_raw_data_parsing
    Styling changes and commented unneccessary code

--- run_player_raw_data_parse.py
+++ run_player_raw_data_parse.py
@@ -8,9 +8,8 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logging.info("Starting run_player_raw_data_parse.py")
 
 
-#file_location = DownloadFileFromS3("player-raw-data")
-
-file_location = '.\\crawled_players\\player-raw-data-2020-01-11.txt'
+file_location = DownloadFileFromS3("player-raw-data")
+#file_location = '.\\crawled_players\\player-raw-data-2020-01-12.txt'
 
 all_parsed_data = []
 
@@ -21,17 +20,14 @@ with open(file_location, "r") as data:
         id = json_data["pdga_number"]
         raw_data = json_data["raw_data"]
         parsed_data = PlayerParseRawData(id, raw_data, '')
-
         all_parsed_data.append(parsed_data)
-        #import pdb; pdb.set_trace()
-
 
 saved_file_location = SaveFile("player", "parse", all_parsed_data)
 
 send_multipart_file_to_s3(saved_file_location, "player-parsed-data")
 
 
-print(json.dumps(all_parsed_data, indent=4))
+#print(json.dumps(all_parsed_data, indent=4))
 
 
 #logging.info('Finished parse - parsed ' + str(len(data_from_file)) + ' players')
