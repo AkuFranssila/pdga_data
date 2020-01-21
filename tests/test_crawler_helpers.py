dec371e705564b4a586bfb2c8ef53d1388f9d86a
AkuFranssila <akufra@gmail.com>
2020-01-21 19:47:15 +0200

    fix-bad_import_on_tests
    Bad import on crawler helper tests

--- tests/test_crawler_helpers.py
+++ tests/test_crawler_helpers.py
@@ -5,8 +5,6 @@ import sys
 import os
 cwd = os.getcwd()
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
-from crawl_player import CrawlPlayer
-from crawl_tournament import CrawlTournament
 from helpers_crawler import FindNewestMemberId, TournamentDate, TournamentLastPage
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
