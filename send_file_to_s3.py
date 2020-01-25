5e06a890ade9827a046e90919cb1eb7186ab41d7
AkuFranssila <akufra@gmail.com>
2020-01-25 16:39:57 +0200

    feature-refactor_s3_send
    change s3 send to use fileobj upload

--- send_file_to_s3.py
+++ send_file_to_s3.py
@@ -22,28 +22,24 @@ def check_if_file_exists_s3(client, key):
 
 def send_file_to_s3(filename, type):
     s3 = AWS_S3CLIENT()
-    test_data = [{"player":"test1"},{"player":"test2"}]
-
-
-    data_to_s3 = []
-    with open(filename, "r") as file_data:
-        for single_line in file_data:
-            json_data = json.loads(single_line)
-            data_to_s3.append(json_data)
-
-    binary_data = bytes(json.dumps(data_to_s3).encode('UTF-8'))
-
     accepted_types = ["old_pdga_data", "player-parsed-data", "player-raw-data", "tournament-parsed-data", "tournament-raw-data"]
 
     if type not in accepted_types:
         logging.critical("Type is not accepted value, check accepted types: %s" % ", ".join(accepted_types))
         return False
     else:
+
+        all_data = []
+        with open(filename, "r") as data:
+            logging.info("Opening file %s" % filename)
+            for line in data:
+                all_data.append(line)
+
         file_extension = ".txt"
         data_location = type + "/" + type + filename.split(type)[-1].rsplit('.', 1)[0] + file_extension
         #data_location = type + "/" + filename.split('\\')[-1].rsplit('.', 1)[0] + file_extension
         logging.info('Sending data to S3 bucket pdga-project-data, location of the file is %s' % data_location)
-        s3.put_object(Bucket="pdga-project-data", Key=data_location, Body=binary_data)
+        s3.put_object(Bucket="pdga-project-data", Key=data_location, Body=all_data)
 
         logging.info("Checking if file exists on S3 we just sent")
         if check_if_file_exists_s3(s3, data_location) > 0:
@@ -54,34 +50,39 @@ def send_file_to_s3(filename, type):
             return False
 
 def send_multipart_file_to_s3(filename, type):
-    s3 = AWS_S3CLIENT()
-    data_to_s3 = []
-    file_extension = ".txt"
-    with open(filename, "r") as file_data:
-       for single_line in file_data:
-           json_data = json.loads(single_line)
-           data_to_s3.append(json_data)
-
-    #binary_data = bytes(json.dumps(data_to_s3).encode('UTF-8'))
-
-    #'.\\crawled_players\\player-raw-data-2020-01-08.json'
-    local_data_location = filename.rsplit('\\', 1)[0] + "\\" + type + filename.split(type)[-1].rsplit('.', 1)[0] + file_extension
-    s3_data_location = type + "/" + type + filename.split(type)[-1].rsplit('.', 1)[0] + file_extension
-
-    with open(local_data_location, 'w') as file:
-        json.dump(data_to_s3, file)
-
+    logging.info("Starting send_multipart_file_to_s3")
     accepted_types = ["old_pdga_data", "player-parsed-data", "player-raw-data", "tournament-parsed-data", "tournament-raw-data"]
 
-    if type not in accepted_types:
+    file_ending = filename.rsplit('.', 1)[1]
+    if file_ending == "json":
+        file_extension = ".json"
+    else:
+        file_extension = ".txt"
+    #local_data_location = filename.rsplit('\\', 1)[0] + "\\" + type + filename.split(type)[-1].rsplit('.', 1)[0] + file_extension
+
+    for type in accepted_types:
+        if type in filename:
+            break
+        else:
+            type = ""
+
+    if type == "":
         logging.critical("Type is not accepted value, check accepted types: %s" % ", ".join(accepted_types))
         return False
-    else:
-        s3.upload_file(filename, "pdga-project-data", s3_data_location)
-        return True
+    s3_data_location = type + "/" + type + filename.split(type)[-1].rsplit('.', 1)[0] + file_extension
+    logging.info("Starting s3 upload")
+
+    with open(filename, "rb") as f:
+        s3 = AWS_S3CLIENT()
+        s3.upload_fileobj(f, "pdga-project-data", s3_data_location)
+
+    logging.info("File uploaded to S3")
+    #s3.upload_file(filename, "pdga-project-data", s3_data_location)
+    return True
 
 
 #latest_file = FindFiles('crawled_players').find_latest_file()
 #print(latest_file)
-#send_file_to_s3('.\\crawled_players\\player-raw-data-2020-01-08.json', "player-raw-data")
-#send_multipart_file_to_s3('.\\crawled_players\\player-raw-data-2020-01-11.txt', "player-raw-data")
+#send_file_to_s3('.\\crawled_players\\player-raw-data-2020-01-24.txt', "player-raw-data")
+#send_multipart_file_to_s3('.\\crawled_players\\player-raw-data-2020-01-12.txt', "player-raw-data")
+#send_file_to_s3('.\\crawled_players\\player-raw-data-2020-01-24.txt', "player-raw-data")
