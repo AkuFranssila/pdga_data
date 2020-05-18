9e7780263bd7a79bb2d6a06715b493aa24bc7289
AkuFranssila <akufra@gmail.com>
2020-05-18 15:35:37 +0300

    refactor tournament parsing to use new argparse and sending logic

--- project/tournament_processes/run_tournament_crawl.py
+++ project/tournament_processes/run_tournament_crawl.py
@@ -2,10 +2,30 @@
 import json
 import logging
 import sys
-from datetime import date
+import datetime
+import argparse
 from project.tournament_processes.tournament_crawl_raw_data import TournamentCrawlRawData
-from project.helpers.helpers_data_management import SaveFile, ReturnFileLocation
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+from project.utils.slack_message_sender import SendSlackMessageToChannel
+logging.getLogger().setLevel("INFO")
 
-file_loc = ReturnFileLocation("tournament", "crawl")
-tournament_data = TournamentCrawlRawData("all", file_loc)
+def handle_arguments() -> (str):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--crawl_options',
+        type=str,
+        choices=["all", "latest", "test"],
+        help="Predefined options what to crawl. Check project\helpers\helpers_crawler.py for more info."
+    )
+
+    args = parser.parse_args()
+
+    return args.crawl_options
+
+
+if __name__ == "__main__":
+    crawl_options = handle_arguments()
+    SendSlackMessageToChannel("%s Starting run_tournament_crawl.py" % str(datetime.datetime.today()), "#data-reports")
+
+    file_date = datetime.datetime.now().strftime("%m%d%Y")
+    TournamentCrawlRawData(crawl_options, file_date)
+
+    SendSlackMessageToChannel("%s Finished run_tournament_crawl.py." % (str(datetime.datetime.today())), "#data-reports")
--- project/tournament_processes/tournament_crawl_raw_data.py
+++ project/tournament_processes/tournament_crawl_raw_data.py
@@ -3,19 +3,17 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import logging
-from datetime import date
+import datetime
 from project.helpers.helpers_crawler import TournamentDate, TournamentLastPage
-from project.helpers.helpers_data_management import AppendToFile, ReturnFileLocation, DeleteFile
-from project.utils.send_file_to_s3 import send_multipart_file_to_s3
+from project.utils.s3_tools import save_to_temp_file_and_upload_to_s3
 from project.utils.slack_message_sender import SendSlackMessageToChannel
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 SendSlackMessageToChannel("%s Starting tournament_crawl_raw_data.py" % str(datetime.datetime.today()), "#data-reports")
 
-def TournamentCrawlRawData(options, file_location):
+def TournamentCrawlRawData(options, file_date, chunk_counter=0):
     url = TournamentDate(options)
     last_page = TournamentLastPage(url)
-    tournament_data = []
     tournament_links = []
     for i in range(0, last_page):
         response = requests.get(url + '&page=' + str(i))
@@ -26,22 +24,31 @@ def TournamentCrawlRawData(options, file_location):
                 link = "https://www.pdga.com" + link['href']
                 if 'https://www.pdga.com/tour/event/' in link and link not in tournament_links and "/global-event/results/" not in link:
                     tournament_links.append(link)
-                    #print ("https://www.pdga.com" + link['href'])
             except:
                 None
 
     logging.info(f'Number of tournaments found {str(len(tournament_links))}')
     SendSlackMessageToChannel("%s Tournament link collection done. Found %s tournament links." % (str(datetime.datetime.today()), str(len(tournament_links))), "#data-reports")
 
+    collected_json = []
 
     for count, link in enumerate(tournament_links):
         logging.info(f'Parsing tournament number {str(count)}/{str(len(tournament_links))}')
         response = requests.get(link)
         data = response.content.decode('utf8').replace("'", '"')
-        json_data = {"pdga_number" : int(link.rslit("/", 1)[1]), "raw_data" : data}
-        AppendToFile(file_location,  json_data)
+        json_data = {"pdga_number" : int(link.rsplit("/", 1)[1]), "raw_data" : data}
 
-    file_send_status = send_multipart_file_to_s3(file_location, "tournament-raw-data")
-    SendSlackMessageToChannel("%s Finished tournament_crawl_raw_data.py." % (str(datetime.datetime.today())), "#data-reports")
-    #if file_send_status:
-    #    DeleteFile(file_location)
+        collected_json.append(json_data)
+
+        print(count)
+        print(len(tournament_links))
+
+        if (count % 1000 == 0 or count == (len(tournament_links) - 1)) and count > 0:
+            save_to_temp_file_and_upload_to_s3("tournament-raw-data", file_date, chunk_counter, collected_json, suffix=".json")
+            collected_json = []
+            chunk_counter += 1
+
+
+    SendSlackMessageToChannel("%s Tournament page crawling and S3 upload done." % (str(datetime.datetime.today())), "#data-reports")
+
+    
